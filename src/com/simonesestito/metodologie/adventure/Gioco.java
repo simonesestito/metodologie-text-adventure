02f273f7f57bb1a2c5ef128547b71735439b4b8c
Simone Sestito <simone@simonesestito.com>
2021-05-25 12:59:01 +0200

    Gioco skeleton

---
+++ src/com/simonesestito/metodologie/adventure/Gioco.java
@@ -0,0 +1,15 @@
+package com.simonesestito.metodologie.adventure;
+
+import com.simonesestito.metodologie.adventure.entita.pojo.Mondo;
+
+import java.nio.file.Path;
+
+public class Gioco {
+    public void play(Mondo mondo) {
+        // TODO
+    }
+
+    public void play(Mondo mondo, Path script) {
+        // TODO
+    }
+}
