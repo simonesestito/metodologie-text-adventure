9ca032ccd93399594aa8225b96f9b152462cea3e
Simone Sestito <simone@simonesestito.com>
2021-07-02 10:56:25 +0200

    Cane parla

--- src/it/uniroma1/textadv/entita/pojo/characters/Cane.java
+++ src/it/uniroma1/textadv/entita/pojo/characters/Cane.java
@@ -1,15 +1,22 @@
 package it.uniroma1.textadv.entita.pojo.characters;
 
 import it.uniroma1.textadv.entita.pojo.features.Accarezzabile;
+import it.uniroma1.textadv.entita.pojo.features.Parla;
 
-public class Cane extends Personaggio implements Accarezzabile {
+public class Cane extends Personaggio implements Accarezzabile, Parla {
+    private static final String ABBAIO = "Bau";
     public Cane(String name) {
         super(name);
     }
 
     @Override
     public void accarezza() {
-        System.out.println("Bau");
+        System.out.println(ABBAIO);
+    }
+
+    @Override
+    public String parla() {
+        return ABBAIO;
     }
 
     @Override
