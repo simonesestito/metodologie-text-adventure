2a8d1098d50e5e0981af6e11bc3231205fd99b44
Simone Sestito <simone@simonesestito.com>
2021-07-08 08:53:32 +0200

    Export packages

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,18 @@
 /**
  * Gioco testuale di avventura, in giro per vari posti e personaggi
  */
-module TextAdventure {
+module it.uniroma1.textadv {
+    exports it.uniroma1.textadv;
+    exports it.uniroma1.textadv.entity.parser;
+    exports it.uniroma1.textadv.entity.factory;
+    exports it.uniroma1.textadv.entity.pojo;
+    exports it.uniroma1.textadv.entity.pojo.characters;
+    exports it.uniroma1.textadv.entity.pojo.objects;
+    exports it.uniroma1.textadv.entity.pojo.features;
+    exports it.uniroma1.textadv.entity.pojo.links;
+    exports it.uniroma1.textadv.engine;
+    exports it.uniroma1.textadv.locale;
+
     requires java.datatransfer;
     requires java.desktop;
 }
